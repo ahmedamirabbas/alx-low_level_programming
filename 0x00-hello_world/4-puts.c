eef063c6d83817569dc67835e6c045328dd0b5f4
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-13 05:09:41 -0700

    Mandatory tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Description: prints "\"Programming is like building a multilingual puzzle" */
+
+int main(void)
+{
+	pust("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Desscription: prints "\"with proper grammar, but the outcome is a piece of art,"*/
+
+int main(void)
+{
+	printf(%s,"with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}

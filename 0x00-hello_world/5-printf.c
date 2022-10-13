5a37bf556a983965479647a86767791a52f7d694
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-13 05:45:09 -0700

    printf 3rd fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/** 
+/**
  * main: Entry point
  * Desscription: prints "\"with proper grammar,
  *  but the outcome is a piece of art,"
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

df284e407f89d4e30bfbdeb6c16a0abbb73cc918
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-13 06:40:07 -0700

    Fix 100 and 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include<unistd.h>
 
 /**
- *
- * main - prints the standrad error sandwiched around the  standard outpu/t
+ * Description main - prints the standrad error sandwiched around 
+ * the standard outpu/t
  * Return: returns 1
  */
 

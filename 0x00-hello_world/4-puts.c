157ebf62a13cbc75a5fc389e6948f8277e36e8a8
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-13 05:14:53 -0700

    puts 2nd fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
-/* Description: prints "\"Programming is like building a multilingual puzzle" */
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
 
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }

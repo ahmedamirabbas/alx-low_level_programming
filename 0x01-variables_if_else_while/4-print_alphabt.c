e8933eabda8a88005dc9adb948237a64935d0f3d
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-14 12:13:59 -0700

    print alphabt and numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return - Always 0 (success/Correct)
+ */
+
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'q')
+			continue;
+
+		else if (alpha == 'e')
+			continue;
+
+		putchar (alpha);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		printf ("%i", num)
+	}
+
+	printf ("\n\);
+
+	return (0);
+
+}

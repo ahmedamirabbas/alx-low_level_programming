c53df8fb5cff8fe2fd954d91886baa1380c8c047
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-14 14:51:00 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,19 @@ int main(void)
 	int number_left;
 	int number_right;
 
-	for (number_left = 48; number_right <= 78; number_left++)
+	for (number_right = 0; number_right <= '9'; number_right++)
 	{
-		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+		for (number_left = number_right + 1 ; number_left <= '9'; number_left++)
 		{
 
 			putchar(number_left);
 			putchar (number_right);
 
-			if ((number_left == 56) && (number_right == 78))
+			if ((number_right != '8' || number_left != '9')
 			{
-				break;
-			}
-
 			putchar(',');
 			putchar (' ');
+			}
 
 		}
 

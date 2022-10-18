df064f97a101bbab111c2f4c9950737287b2b032
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-18 15:14:31 -0700

    directory files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * main - prints _putchar followed by a new line
+ * return: returns 0 (success/correct)
+ */
+
+int main (void)
+{
+	_putchar('_putchar\n');
+
+	return (0);
+}

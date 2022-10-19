6e4061efee7907315a0548b37145931af989dbd7
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-18 18:37:01 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,12 +7,12 @@
  * Return: always 0
  */
 
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n < 98)
 	{
 		for (n = n; n < 98; n++)
-			printf("%\n", 98);
+			printf("%d, ", 98);
 	}
 	else
 	{

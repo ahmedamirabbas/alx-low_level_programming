03b6329a0fd229d8e77dde6c925736cd7d2c683f
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-18 18:44:58 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: the intital number 
+ * @n: the intital number
  * Return: always 0
  */
 
@@ -12,12 +12,16 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		for (; n > 98; n--)
+		{
 			printf("%d, ", n);
+		}
 	}
 	else if (n < 98)
 	{
 		for (; n > 98; n++)
+		{
 			printf("%d, ", n);
-		printf("98\n");
+		}
 	}
+		printf("98\n");
 }

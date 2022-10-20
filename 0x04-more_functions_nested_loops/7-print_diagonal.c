411323fe86fc50f34359d89833dba3344f1482e7
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-20 12:39:29 -0700

    print diagonal fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,24 +9,21 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 
 	else
 	{
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-		}
-
 		_putchar('\n');
-		}
 	}
 }

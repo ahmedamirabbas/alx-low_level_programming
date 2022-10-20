98f3fc182a98f8f28c81ae968241e59c703bc2b7
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-20 08:50:42 -0700

    task 4 fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,12 +11,7 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if ((i == 2) || (i == 4))
-		{
-			break;
-		}
-
-		else
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}

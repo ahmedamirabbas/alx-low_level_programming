13a5c5099705b9d6e20ce5a99b912121b733f36b
amedo579 <ahmad.abdulkhalig@gmail.com>
2022-10-20 09:14:02 -0700

    101 betty fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - print sum of multiples of 3 or 5 below 1024
- *
- *  * Return: 0
- *
- *  */
-
-
-
+ * main - print sum of multiples of 3 or 5 below 1024
+ * Return: 0
+ */
 int main(void)
-
 {
-
 	int counter = 1024;
-
 	int i;
-
 	int sum = 0;
 
 	for (i = 0; i < counter; i++)
-
 	{
-
 		if (i % 5 == 0 || i % 3 == 0)
-
 		{
-
 			sum += i;
-
 		}
-
 	}
 
 	printf("%d\n", sum);
 
 	return (0);
-
 }

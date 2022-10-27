30ba302fac09743c35d89c5f56f8ff734bb9441a
ahmedamirabbas <ahmad.abdulkhalig@gmail.com>
2022-10-27 11:45:28 -0700

    strcat fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++])
+	while (dest[index++])
 		j++;
-	for (i = 0; src[i]; i++)
-		dest[j++] = src[i];
+	for (index = 0; src[index]; index++)
+		dest [dest_len++] = src[index];
 	return (dest);
 }

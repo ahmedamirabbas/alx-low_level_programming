8fcc950bfc2d9b1e1eb18175fa63e8dfe1c38adc
ahmedamirabbas <ahmad.abdulkhalig@gmail.com>
2022-11-24 06:21:27 -0800

    initial commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,37 @@
  * @n: number of the times table
  */
 void print_times_table(int n)
-{
-	int i, j, k;
+{       int i, j, k;
 
 	if (n >= 0 && n <= 14)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
+			{       k = j * i;
 				if (k > 99)
-				{
+				{       
 					_putchar(',');
 					_putchar(32);
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-
-				else if (k > 9)
-				{
+				} else if (k > 9)
+				{       
 					_putchar(',');
 					_putchar(32);
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-				}
-
-				else
+				} else
 				{
 					{
 						if (j != 0)
-							_putchar(',');
+					        _putchar(',');
 						_putchar(32);
 						_putchar(32);
 						_putchar(32);
 					}
-
 					_putchar(k + '0');
 				}
 			}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # alx-low_level_programming
-My first Repo learning C
+This repository contains programs written for low-level programming and algorithms. I update this repo with my learning progresses, which includes the following projects:
+0x00. C - Hello, World
